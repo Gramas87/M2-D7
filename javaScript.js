f7d7d1801e9db4e16048e55607e73d3173b77062
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2022-01-11 17:01:24 +0100

    more done

--- javaScript.js
+++ javaScript.js
@@ -2,3 +2,10 @@ const newLink = function () {
    let nav = document.querySelector(".nav-scroller")
     
 }
+
+
+
+colorGreen = function () {
+document.querySelector("h1").style.color="green"
+}
+console.log(colorGreen)
